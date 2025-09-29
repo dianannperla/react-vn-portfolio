664a76bf043ce2dfc3059df11258e93baae02552
dianannperla <waafirmelyza@hotmail.com>
2025-09-29 15:20:50 +0100

    feat: initial commit for src/components/Projects.js

---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Projects.css';
+
+function Projects() {
+  return (
+    <div className='projects'>
+      <h2>Các Dự Án Của Tôi</h2>
+    </div>
+  );
+}
+
+export default Projects;
