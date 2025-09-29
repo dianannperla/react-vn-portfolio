38663f73cac62f0fa8f52572451f308848344924
dianannperla <waafirmelyza@hotmail.com>
2025-09-29 15:20:49 +0100

    feat: initial commit for src/components/Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className='home'>
+      <h1>Chào Mừng Đến Với Portfolio Cá Nhân Của Tôi</h1>
+    </div>
+  );
+}
+
+export default Home;
